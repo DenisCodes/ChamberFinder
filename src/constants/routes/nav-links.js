009ae52db625db13e4df7e4961a57284e87aecf4
deniscodes <smirnovadenis@gmail.com>
2022-11-30 20:49:44 -0500

    initial commit

---
+++ src/constants/routes/nav-links.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    name: "Home",
+    to: "/",
+  },
+  {
+    name: "Listing",
+    to: "/listing",
+  },
+  {
+    name: "Landlords",
+    to: "/agents",
+  },
+  {
+    name: "login",
+    to: "/login",
+  },
+];
+
+export default links;
